1af098adb32a5c1011365e8fb59cf0c19288a6f9
Mikhail Drozdov <deathlokmike@gmail.com>
2024-06-26 12:31:20 +0300

    ✅ Updated tests

---
+++ test/cameraHttpTest.cpp
@@ -0,0 +1,41 @@
+#include <HTTPClient.h>
+#include <Wire.h>
+
+#include "Config.h"
+#include "OV7670.h"
+
+OV7670 *camera;
+HTTPClient http;
+
+void setup() {
+    Serial.begin(USB_SPEED);
+    Wire.begin();
+    Wire.setClock(1000000);
+    uint32_t memBefore = ESP.getFreeHeap();
+    camera = new OV7670(CAM_VSYNC, CAM_HREF, CAM_XCLK, CAM_PCLK, CAM_D0, CAM_D1,
+                        CAM_D2, CAM_D3, CAM_D4, CAM_D5, CAM_D6, CAM_D7);
+    ESP_LOGD(mainLogTag, "Memory allocated: %d", memBefore - ESP.getFreeHeap());
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+}
+
+void loop() {
+    if ((WiFi.status() == WL_CONNECTED)) {
+        camera->oneFrame();
+        http.begin(endpoint);
+
+        int httpResponseCode =
+            http.sendRequest("POST", camera->frame, camera->frameBytes);
+
+        if (httpResponseCode > 0) {
+            String payload = http.getString();
+        } else {
+        }
+        http.end();
+    } else {
+    }
+}
--- test/cameraTest.cpp
+++ test/cameraTest.cpp
@@ -1,57 +1,21 @@
-#include "OV7670.h"
+#include <Wire.h>
+
 #include "Config.h"
-#include <Arduino.h>
-#include <HTTPClient.h>
+#include "OV7670.h"
+
 OV7670 *camera;
-String endpoint = "http://192.168.1.163:8000/image";
 
-void setup()
-{
+void setup() {
     Serial.begin(USB_SPEED);
-
-    camera = new OV7670(CAM_VSYNC, CAM_HREF, CAM_XCLK, CAM_PCLK,
-                        CAM_D0, CAM_D1, CAM_D2, CAM_D3,
-                        CAM_D4, CAM_D5, CAM_D6, CAM_D7);
-    Serial.println("Идет подключение к Wi-Fi");
-    WiFi.begin(ssid, password);
-
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        delay(500);
-        Serial.print(".");
-    }
-    Serial.println("Соединение с Wi-Fi установлено");
+    Wire.begin();
+    Wire.setClock(100000);
+    uint32_t memBefore = ESP.getFreeHeap();
+    camera = new OV7670(CAM_VSYNC, CAM_HREF, CAM_XCLK, CAM_PCLK, CAM_D0, CAM_D1,
+                        CAM_D2, CAM_D3, CAM_D4, CAM_D5, CAM_D6, CAM_D7);
+    ESP_LOGD(mainLogTag, "Memory allocated: %d", memBefore - ESP.getFreeHeap());
 }
 
-void loop()
-{
-    if ((WiFi.status() == WL_CONNECTED))
-    {
-        HTTPClient http;
-        Serial.println("Делается снимок");
-        camera->oneFrame();
-        Serial.println("Снимок сохранен");
-        String url = endpoint;
-        http.begin(url.c_str());
-
-        int httpResponseCode =
-            http.sendRequest("POST", camera->frame, camera->frameBytes);
-
-        if (httpResponseCode > 0)
-        {
-            String payload = http.getString();
-            Serial.println(httpResponseCode);
-            Serial.println(payload);
-        }
-        else
-        {
-            Serial.println("Ошибка HTTP-запроса");
-            Serial.println(httpResponseCode);
-        }
-        http.end();
-    }
-    else
-    {
-        Serial.println("WiFi Disconnected");
-    }
+void loop() {
+    camera->oneFrame();
+    delay(2000);
 }
